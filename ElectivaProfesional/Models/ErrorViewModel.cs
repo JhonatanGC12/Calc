fe1b652ce95207326c6b35bdbc105624155214e6
xXFOXXx12 <jhonatana.galindoc@autonoma.edu.co>
2024-02-11 10:01:52 -0500

    calculadora

---
+++ ElectivaProfesional/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ElectivaProfesional.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
