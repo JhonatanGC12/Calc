ec3b342462df3c45426fd3ff5131c16e8eef7077
xXFOXXx12 <jhonatana.galindoc@autonoma.edu.co>
2024-02-11 10:18:15 -0500

    Modelo de la calculadora

---
+++ ElectivaProfesional/Models/Calculadora.cs
@@ -0,0 +1,11 @@
+namespace ElectivaProfesional.Models;
+
+public class CalculadoraViewModel
+{
+    public int Opr1 { get; set; }
+    public int Opr2 { get; set; }
+    public string Action { get; set; }
+    public int Respuesta { get; set; }
+
+    
+}
